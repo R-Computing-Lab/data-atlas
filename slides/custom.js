68ff46b3f8742b2b4227cc37e423c026a7db98f0
Mason Garrison <garrissm@wfu.edu>
2024-06-23 11:40:16 -0400

    moved slides to seperate repo

--- slides/custom.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var hiddenSlides = document.getElementsByClassName('my-hidden-slide');
-  for (var i = 0; i < hiddenSlides.length; i++) {
-    hiddenSlides[i].style.display = 'none';
-  }
-});
